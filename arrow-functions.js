8a35e36910aa2e7d27b1b99dc7aa5065fad2e38e
GitMulC <cmulvo@gmail.com>
2022-02-16 22:21:17 +0000

    Add function examples

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,38 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers1(a, b) {
+    //Code block
+    return a + b;
+}
+let sum1 = addTwoNumbers1(3,5);
+console.log(sum1);
 
 // Arrow Function With Parameters
+const addTwoNumbers2 = (a,b) => {
+    // Code block
+    return a + b;
+}
+let sum2 = addTwoNumbers2(3,5);
+console.log(sum2);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers3 = (a, b) => a + b;
+const addTwoNumbers4 = (a, b) => (a + b); //also valid
+let sum3 = addTwoNumbers3(6, 4);
+console.log(sum3)
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!!')
+
+const sayHello = () => console.log('hello')
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+        This is a multi line string!
+    </p>`
+)
+console.log(returnMultipleLines());
